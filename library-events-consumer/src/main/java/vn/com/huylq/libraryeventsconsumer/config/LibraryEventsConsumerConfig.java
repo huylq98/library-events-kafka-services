5187620e4586349de0e156378becec81f50753de
huylq33 <huylq33@viettel.com.vn>
2022-06-28 19:40:55 +0700

    Base consumer

---
+++ library-events-consumer/src/main/java/vn/com/huylq/libraryeventsconsumer/config/LibraryEventsConsumerConfig.java
@@ -0,0 +1,9 @@
+package vn.com.huylq.libraryeventsconsumer.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.annotation.EnableKafka;
+
+@Configuration
+@EnableKafka
+public class LibraryEventsConsumerConfig {
+}
